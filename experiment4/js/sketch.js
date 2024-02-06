8d54eb03e3d42686c5ef020b7c8a62f2a6e45128
Samip Niraula <samiprichardniraula@gmail.com>
2024-02-05 18:58:55 -0800

    Added code and changed indexes

--- experiment4/js/sketch.js
+++ experiment4/js/sketch.js
@@ -1,67 +1,50 @@
-// sketch.js - purpose and description here
-// Author: Your Name
-// Date:
+let capture, video;
+let vScale = 10;
 
-// Here is how you might set up an OOP p5.js project
-// Note that p5.js looks for a file called sketch.js
-
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
-
-// Globals
-let myInstance;
-let canvasContainer;
-
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
-
-    myMethod() {
-        // code to run when method is called
-    }
-}
-
-// setup() function is called once when the program starts
 function setup() {
-    // place our canvas, making it fit our container
-    canvasContainer = $("#canvas-container");
-    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
-    canvas.parent("canvas-container");
-    // resize canvas is the page is resized
-    $(window).resize(function() {
-        console.log("Resizing...");
-        resizeCanvas(canvasContainer.width(), canvasContainer.height());
-    });
-    // create an instance of the class
-    myInstance = new MyClass(VALUE1, VALUE2);
-
-    var centerHorz = windowWidth / 2;
-    var centerVert = windowHeight / 2;
+	createCanvas(windowWidth, screen.availHeight);
+	capture = createCapture(VIDEO);
+	capture.size(width / vScale, height / vScale);
+	capture.hide();
+	video = createGraphics(width, height);
 }
 
-// draw() function is called repeatedly, it's the main animation loop
 function draw() {
-    background(220);    
-    // call a method on the instance
-    myInstance.myMethod();
+	background('black');
+	
+	video.scale(-1, 1);
+	video.image(capture, 0, 0, -width, height);
+	video.loadPixels();
+	
+	for (let y = vScale / 2; y < height; y += vScale) {
+		for (let x = vScale / 2; x < width; x += vScale) {
+			let index = (y * video.width + x) * 4;
+			let r = video.pixels[index + 0];
+			let g = video.pixels[index + 1];
+			let b = video.pixels[index + 2];
 
-    // Put drawings here
-    var centerHorz = canvasContainer.width() / 2 - 125;
-    var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
+			let bright = (r + g + b) / 3; // 0 - 255
+			fill(bright);
+			noStroke();
+			
+			let d = map(bright, 0, 255, 0, vScale);
+			rectMode(CENTER);
+			rect(x, y, d, d);
+		}
+	}
+  	let gridSize =8;
+	
+    video.loadPixels();
+	for (let y=0; y<video.height; y+=gridSize) {
+		for (let x=0; x<video.width; x+=gridSize) {
+			
+    let index = (y * video.width + x) * 4;
+    let r = video.pixels[index];
+    let dia = map(r,0,255,gridSize,2);
+			
+    fill(0);
     noStroke();
-    rect(centerHorz, centerVert, 250, 250);
-    fill(255);
-    textStyle(BOLD);
-    textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
-}
-
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
+    circle(x+gridSize/2, y+gridSize/2,dia);
+		}
+	}
 }
