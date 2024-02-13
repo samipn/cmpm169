72f0d9c548532fc9ed3b3dd29ab31935668c6d56
Samip Niraula <samiprichardniraula@gmail.com>
2024-02-12 17:03:44 -0800

    fixing code

--- experiment5/js/sketch.js
+++ experiment5/js/sketch.js
@@ -1,37 +1,35 @@
 function setup() {
     createCanvas(windowWidth, windowHeight, WEBGL);
     noStroke();
-    frameRate(240)
-    colorMode(HSB)
-    noFill()
-    strokeWeight(8)
+    colorMode(HSB);
+    noFill();
+    strokeWeight(8);
   }
   
-  function draw() {
+function draw() {
       background(255);
-        orbitControl();
+      orbitControl();
       noFill();
       stroke('brown');
       strokeWeight(0.5);
       
       sphere(50);
     
-        orbitControl();                                          
+      orbitControl();                                          
       rotateY(0.5);
       for (let i = 0; i < 385; i = i + 5){                     
            stroke('white');                                    
            strokeWeight(8);                                     
-           line(-600*sin(i*PI/12)*cos(i*PI/60), -600*sin(i*PI/12)*sin(i*PI/90), 650, 350*cos(i*PI/       45), 350*sin(i*PI/45), -450);
+           line(-600*sin(i*PI/12)*cos(i*PI/60), -600*sin(i*PI/12)*sin(i*PI/90), 650, 350*cos(i*PI/45), 350*sin(i*PI/45), -450);
       }
     
       orbitControl();
     
       curved(30, 12)
-      curved(-30, -12)
-    
-  }
+      curved(-30, -12)   
+}
   
-  function curved(y, speed) {
+function curved(y, speed) {
       beginShape()
     for (let i = 0; i < 300; i++) {
       stroke(i % 60, 100, 80)
@@ -40,4 +38,4 @@ function setup() {
              sin(frameCount / speed + i / 3) * map(sin(i * 0.05 + frameCount / 32), -1, 1, 60, 300))
     }
     endShape()
-  }
+}
